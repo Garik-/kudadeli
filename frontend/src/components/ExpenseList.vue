69600a8adb7a6b0be93b6aebd48e39b3a6ac019a
Gar|k <garik.djan@gmail.com>
2025-07-18 01:43:17 +0300

    test app

---
+++ frontend/src/components/ExpenseList.vue
@@ -0,0 +1,24 @@
+<script lang="ts">/*
+type Expense struct {
+	ID          ExpenseID       `json:"id"`
+	CreatedAt   time.Time       `json:"createdAt"`
+	UpdatedAt   time.Time       `json:"updatedAt"`
+	Category    Category        `json:"category"`
+	PaymentType PaymentType     `json:"paymentType"`
+	Description string          `json:"description"`
+	Amount      decimal.Decimal `json:"amount"`
+	UserID      int64           `json:"userId"`
+}
+*/
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Expense</h3>
+    </div>
+    <div class="card-body">
+      dfdgdfg
+    </div>
+  </div>
+</template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,4 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import '@tabler/core/dist/css/tabler.min.css'
+import '@tabler/core/dist/js/tabler.min.js'
+
 createApp(App).mount('#app')
